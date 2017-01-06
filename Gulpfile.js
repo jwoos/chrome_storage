07cd009691fec4ecbd4a1a05ba0fa11ae06c1f44
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-01-05 23:48:11 -0500

    cleaned up gulp files

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,37 +2,103 @@
 
 const gulp = require('gulp');
 
-const dependencies = {
-	gulp: gulp,
+const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-	// node built ins
-	childProcess: require('child_process'),
-	fs: require('fs'),
-	path: require('path'),
-
-	atl: require('awesome-typescript-loader'),
-	babel: require('gulp-babel'),
-	colors: require('colors/safe'),
-	del: require('del'),
-	eslint: require('gulp-eslint'),
-	imagemin: require('gulp-imagemin'),
-	mergeStream: require('merge-stream'),
-	plumber: require('gulp-plumber'),
-	sass: require('gulp-sass'),
-	sourcemaps: require('gulp-sourcemaps'),
-	ts: require('gulp-typescript'),
-	tslint: require('gulp-tslint'),
-	webpack: require('webpack-stream'),
-	wp: require('webpack'),
-};
+const atl = require('awesome-typescript-loader');
+const babel = require('gulp-babel');
+const colors = require('colors/safe');
+const del = require('del');
+const eslint = require('gulp-eslint');
+const imagemin = require('gulp-imagemin');
+const mergeStream = require('merge-stream');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
+const webpack = require('webpack-stream');
+const wp = require('webpack');
 
 const config = {
 	envs: ['srv', 'dist']
 };
 
-// Modular task registration
-dependencies.fs.readdirSync('gulp').forEach((module) => {
-	require(`./gulp/${module}`)(dependencies, config);
+const tsProject = ts.createProject('tsconfig.json');
+
+config.envs.forEach((env) => {
+	gulp.task(`init:${env}`, () => {
+		return new Promise((resolve, reject) => {
+			childProcess.exec(`if [ ! -d build/${env} ]; then mkdir build/${env}; fi`, (e) => {
+				e ? reject() : resolve();
+			});
+		});
+	});
+
+	gulp.task(`copy:${env}`, () => {
+		return gulp.src(['./manifest.json'])
+			.pipe(plumber())
+			.pipe(gulp.dest(`./build/${env}/`));
+	});
+
+	gulp.task(`clean:${env}`, () => {
+		return del([`build/${env}/**/*`]);
+	});
+});
+
+gulp.task('tslint', () => {
+	return gulp.src(['./src/**/*.ts'])
+		.pipe(plumber())
+		.pipe(tslint({
+			formatter: 'verbose'
+		}))
+		.pipe(tslint.report({
+			emitError: false
+		}));
+});
+
+gulp.task('eslint', () => {
+	return gulp.src(['./src/scripts/*.js'])
+		.pipe(plumber())
+		.pipe(eslint({
+			eslint: '.eslintrc.json'
+		}))
+		.pipe(eslint.format());
+});
+
+gulp.task('js:srv', () => {
+	return gulp.src(['./src/**/*.js'])
+		.pipe(plumber())
+		.pipe(gulp.dest('./build/srv/'));
+});
+
+gulp.task('js:dist', () => {
+	return gulp.src(['./src/**/*.js'])
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['babili']
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./build/dist/'));
+});
+
+gulp.task('ts:srv', () => {
+	return gulp.src(['./src/**/*.ts'])
+		.pipe(plumber())
+		.pipe(tsProject())
+		.js.pipe(gulp.dest('./build/srv/'));
+});
+
+gulp.task('ts:dist', () => {
+	return gulp.src(['./src/**/*.ts'])
+		.pipe(plumber())
+		.pipe(tsProject())
+		.js.pipe(babel({
+			presets: ['babili']
+		}))
+		.pipe(gulp.dest('./build/dist/'));
 });
 
 gulp.task('srv', gulp.series('init:srv', 'clean:srv', 'tslint', 'ts:srv'));
--- test/runner.js
+++ test/runner.js
@@ -31,13 +31,13 @@ jasmine.configureDefaultReporter({
 
 jasmine.jasmine.getEnv().addReporter(new specReporter({
 	displayStacktrace: 'specs',    // display stacktrace for each failed assertion, values: (all|specs|summary|none)
-	displayFailuresSummary: true, // display summary of all failures after execution
-	displayPendingSummary: true,  // display summary of all pending specs after execution
-	displaySuccessfulSpec: true,  // display each successful spec
-	displayFailedSpec: true,      // display each failed spec
-	displayPendingSpec: true,    // display each pending spec
-	displaySpecDuration: false,   // display each spec duration
-	displaySuiteNumber: false,    // display each suite number (hierarchical)
+	displayFailuresSummary: true,  // display summary of all failures after execution
+	displayPendingSummary: true,   // display summary of all pending specs after execution
+	displaySuccessfulSpec: true,   // display each successful spec
+	displayFailedSpec: true,       // display each failed spec
+	displayPendingSpec: true,      // display each pending spec
+	displaySpecDuration: false,    // display each spec duration
+	displaySuiteNumber: false,     // display each suite number (hierarchical)
 	colors: {
 		success: 'green',
 		failure: 'red',

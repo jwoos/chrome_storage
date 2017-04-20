8f3a56acf0a35548691c496637f0f9c99336659c
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-04-20 13:07:36 -0400

    pass travis envs

--- scripts/posttest.sh
+++ scripts/posttest.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $1 == 'docker' ]]; then
-	docker run -it --rm -v "$PWD":/usr/src/app -w /usr/src/app jwoos/chrome_storage bash -c 'cat coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js'
+	docker run -it --rm \
+		-v "$PWD":/usr/src/app \
+		-w /usr/src/app \
+		-e "COVERALLS_REPO_TOKEN=${COVERALLS_REPO_TOKEN}" \
+		-e "TRAVIS=${TRAVIS}" \
+		-e "TRAVIS_JOB_ID=${TRAVIS_JOB_ID}" \
+		jwoos/chrome_storage bash -c 'cat coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js'
 else
 	cat ./coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v
 fi
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ $1 == 'docker' ]]; then
-	docker run -it --rm -v "$PWD":/usr/src/app -w /usr/src/app jwoos/chrome_storage xvfb-run ./node_modules/karma/bin/karma start karma.conf.js
+	docker run -it --rm \
+		-v "$PWD":/usr/src/app \
+		-w /usr/src/app \
+		jwoos/chrome_storage xvfb-run ./node_modules/karma/bin/karma start karma.conf.js
 elif [[ $1 == 'xvfb' ]]; then
 	xvfb-run karma start karma.conf.js
 else

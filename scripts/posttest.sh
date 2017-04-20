5b7bec674be06c63d134f83bca3ba6fba4ab1f4f
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-04-20 00:21:56 -0400

    verbose logging

--- scripts/posttest.sh
+++ scripts/posttest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-cat ./coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+cat ./coverage/lcov/lcov.info
+cat ./coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v

55296af0f6c08348bb58056fa12496165b87e5b4
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-04-20 10:36:44 -0400

    use docker for postest as well

--- scripts/posttest.sh
+++ scripts/posttest.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-cat ./coverage/lcov/lcov.info > ./coverage/lcov-own.info
-cat ./coverage/lcov-own.info | ./node_modules/coveralls/bin/coveralls.js -v
+if [[ $1 == 'docker' ]]; then
+	docker run -it --rm -v "$PWD":/usr/src/app -w /usr/src/app jwoos/chrome_storage bash -c 'cat coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js'
+else
+	cat ./coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v
+fi
